663a8d8c17e3f200792c6658191023525ece1f1e
Fernan Marco Tipones <ftipones@legalsight.com>
2022-08-08 18:52:04 +0800

    initial structure

---
+++ src/app/shared/components/show-list/show-list.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ShowListComponent } from './show-list.component';
+
+
+
+@NgModule({
+  declarations: [
+    ShowListComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class ShowListModule { }
